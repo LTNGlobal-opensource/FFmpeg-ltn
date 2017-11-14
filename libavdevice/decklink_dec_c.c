1fe35b6d8520aade6288a9cd4ddb76d8b66ddef6
Devin Heitmueller <dheitmueller@ltnglobal.com>
2017-11-14 16:10:36 -0500

    Add missing file from last commit.
    Needs to be merged with previous patch prior to upstream submission.

--- libavdevice/decklink_dec_c.c
+++ libavdevice/decklink_dec_c.c
@@ -75,6 +75,7 @@ static const AVOption options[] = {
     { "wallclock",     NULL,                                          0,  AV_OPT_TYPE_CONST, { .i64 = PTS_SRC_WALLCLOCK}, 0, 0, DEC, "pts_source"},
     { "draw_bars",     "draw bars on signal loss" , OFFSET(draw_bars),    AV_OPT_TYPE_BOOL,  { .i64 = 1}, 0, 1, DEC },
     { "queue_size",    "input queue buffer size",   OFFSET(queue_size),   AV_OPT_TYPE_INT64, { .i64 = (1024 * 1024 * 1024)}, 0, INT64_MAX, DEC },
+    { "enable_scte_104", "capture SCTE-104 VANC", OFFSET(enable_scte_104), AV_OPT_TYPE_INT, { .i64 = 0 }, 0, 1, DEC, "enable_scte_104"},
     { NULL },
 };
 

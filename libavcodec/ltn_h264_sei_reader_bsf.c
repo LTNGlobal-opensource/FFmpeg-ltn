2d17b048c226c427ae56e217eaaefbc289e67d17
Steven Toth <stoth@kernellabs.com>
2018-08-16 12:47:17 -0400

    Improvements to the ltn_h264_sei_reader debugging filter. Adding averaging, UDP exit time etc.

--- libavcodec/ltn_h264_sei_reader_bsf.c
+++ libavcodec/ltn_h264_sei_reader_bsf.c
@@ -37,12 +37,25 @@
 typedef struct ReaderContext
 {
     int rowcount;
+    unsigned int last_hwrecd_tvsec;
+    struct timeval hwrecd_current_second;
+    double hwrecd_current_avg;
+    double hwrecd_current_second_frames;
 } ReaderContext;
 
 static const unsigned char _uuid[] = 
 {
     0x59, 0x96, 0xff, 0x28, 0x17, 0xca, 0x41, 0x96, 0x8d, 0xe3, 0xe5, 0x3f, 0xe2, 0xf9, 0x92, 0xae
 };
+
+static void timeval_add(struct timeval *result, struct timeval *x, struct timeval *y)
+{
+    result->tv_sec = x->tv_sec + y->tv_sec;
+
+    unsigned int v = x->tv_usec + y->tv_usec;
+    result->tv_sec += (v / 1000000);
+    result->tv_usec = v % 1000000;
+}
 
 static int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y)
 {
@@ -72,10 +85,14 @@ static int ltn_h264_sei_reader_filter(AVBSFContext *ctx, AVPacket *out)
 {
     ReaderContext *s = ctx->priv_data;
     uint32_t frameNumber;
-    struct timeval begin, end, diff;
+    struct timeval hwrecd, codecstart, codecend, codectime, totaltime, udpend;
+    struct timeval encodertime;
     AVPacket *in;
     int ret, i = 0, j;
-    uint32_t x;
+    uint32_t w, x, y, z;
+
+    struct timeval now;
+    gettimeofday(&now, NULL);
 
     ret = ff_bsf_get_packet(ctx, &in);
     if (ret < 0)
@@ -97,54 +114,118 @@ static int ltn_h264_sei_reader_filter(AVBSFContext *ctx, AVPacket *out)
 
             /* Extract fields. */
             j = i + 16;
+
+            /* Field 1: frame number */
             frameNumber  = in->data[j + 0] << 24;
             frameNumber |= in->data[j + 1] << 16;
             frameNumber |= in->data[j + 3] <<  8;
             frameNumber |= in->data[j + 4] <<  0;
 
-            begin.tv_sec  = in->data[j +  6] << 24;
-            begin.tv_sec |= in->data[j +  7] << 16;
-            begin.tv_sec |= in->data[j +  9] <<  8;
-            begin.tv_sec |= in->data[j + 10] <<  0;
+            /* Field 2: Walltime when hardware received (secs) */
+            hwrecd.tv_sec  = in->data[j +  6] << 24;
+            hwrecd.tv_sec |= in->data[j +  7] << 16;
+            hwrecd.tv_sec |= in->data[j +  9] <<  8;
+            hwrecd.tv_sec |= in->data[j + 10] <<  0;
 
-            begin.tv_usec  = in->data[j + 12] << 24;
-            begin.tv_usec |= in->data[j + 13] << 16;
-            begin.tv_usec |= in->data[j + 15] <<  8;
-            begin.tv_usec |= in->data[j + 16] <<  0;
-            x = begin.tv_usec;
+            /* Field 3: Walltime when hardware received (usecs) */
+            hwrecd.tv_usec  = in->data[j + 12] << 24;
+            hwrecd.tv_usec |= in->data[j + 13] << 16;
+            hwrecd.tv_usec |= in->data[j + 15] <<  8;
+            hwrecd.tv_usec |= in->data[j + 16] <<  0;
+            x = hwrecd.tv_usec;
 
-            end.tv_sec  = in->data[j + 18] << 24;
-            end.tv_sec |= in->data[j + 19] << 16;
-            end.tv_sec |= in->data[j + 21] <<  8;
-            end.tv_sec |= in->data[j + 22] <<  0;
+            /* Field 3: Walltime when going into video code (secs) */
+            codecstart.tv_sec  = in->data[j + 18] << 24;
+            codecstart.tv_sec |= in->data[j + 19] << 16;
+            codecstart.tv_sec |= in->data[j + 21] <<  8;
+            codecstart.tv_sec |= in->data[j + 22] <<  0;
 
-            end.tv_usec  = in->data[j + 24] << 24;
-            end.tv_usec |= in->data[j + 25] << 16;
-            end.tv_usec |= in->data[j + 27] <<  8;
-            end.tv_usec |= in->data[j + 28] <<  0;
+            /* Field 4: Walltime when going into video code (usecs) */
+            codecstart.tv_usec  = in->data[j + 24] << 24;
+            codecstart.tv_usec |= in->data[j + 25] << 16;
+            codecstart.tv_usec |= in->data[j + 27] <<  8;
+            codecstart.tv_usec |= in->data[j + 28] <<  0;
+            y = codecstart.tv_usec;
 
-            timeval_subtract(&diff, &end, &begin);
+            /* Field 5: Walltime when frame leaving codec (secs) */
+            codecend.tv_sec  = in->data[j + 30] << 24;
+            codecend.tv_sec |= in->data[j + 31] << 16;
+            codecend.tv_sec |= in->data[j + 33] <<  8;
+            codecend.tv_sec |= in->data[j + 34] <<  0;
+
+            /* Field 6: Walltime when frame leaving codec (usecs) */
+            codecend.tv_usec  = in->data[j + 36] << 24;
+            codecend.tv_usec |= in->data[j + 37] << 16;
+            codecend.tv_usec |= in->data[j + 39] <<  8;
+            codecend.tv_usec |= in->data[j + 40] <<  0;
+            z = codecend.tv_usec;
+
+            if (codecend.tv_sec && codecstart.tv_sec)
+                timeval_subtract(&codectime, &codecend, &codecstart);
+            else
+                memset(&codectime, 0, sizeof(codectime));
+            timeval_subtract(&totaltime, &now, &hwrecd);
+
+            /* Field 7: Walltime when frame leaving udp transmitter (secs) */
+            udpend.tv_sec  = in->data[j + 42] << 24;
+            udpend.tv_sec |= in->data[j + 43] << 16;
+            udpend.tv_sec |= in->data[j + 45] <<  8;
+            udpend.tv_sec |= in->data[j + 46] <<  0;
+
+            /* Field 8: Walltime when frame leaving udp transmitter (usecs) */
+            udpend.tv_usec  = in->data[j + 48] << 24;
+            udpend.tv_usec |= in->data[j + 49] << 16;
+            udpend.tv_usec |= in->data[j + 51] <<  8;
+            udpend.tv_usec |= in->data[j + 52] <<  0;
+            w = udpend.tv_usec;
+
+            timeval_subtract(&encodertime, &udpend, &hwrecd);
+
+            s->hwrecd_current_second_frames++;
+            struct timeval cs = s->hwrecd_current_second;
+            timeval_add(&s->hwrecd_current_second, &totaltime, &cs);
+
+            if (s->last_hwrecd_tvsec != hwrecd.tv_sec) {
+                s->last_hwrecd_tvsec = hwrecd.tv_sec;
+                s->hwrecd_current_avg = s->hwrecd_current_second.tv_sec * 1000000;
+                s->hwrecd_current_avg += s->hwrecd_current_second.tv_usec;
+                s->hwrecd_current_avg /= s->hwrecd_current_second_frames;
+                s->hwrecd_current_avg /= 1000000;
+                s->hwrecd_current_second.tv_sec = 0;
+                s->hwrecd_current_second.tv_usec = 0;
+                s->hwrecd_current_second_frames = 0;
+            }
 
             if (s->rowcount++ == 0) {
-                printf("frame        Codec              Codec              Codec Latency     \n");
-                printf("Number       Entry Time------>  Exit Time------->  Time (Seconds)--->\n");
+                printf("frame        Encoder            Encoder            Codec              Codec             Codec Latency     Walltime minus    Walltime    Encoder Total\n");
+                printf("Number       Entry Time------>  UDP Exit Time----> Entry Time------>  Exit Time-------> Time (Secs)---->  Encoder Entry-->  Average-->  Time (Secs)---->\n");
             }
             if (s->rowcount == 25)
                 s->rowcount = 0;
 
 #ifdef __LINUX__
-            printf("%011d  %09ld.%06u  %09ld.%06lu  %ld.%06lu\n",
+            printf("%011d  %09ld.%06u  %09ld.%06lu  %09ld.%06lu  %09ld.%06lu  %09ld.%06lu  %09ld.%06lu  %010.03f  %09ld.%06lu\n",
                 frameNumber,
-                begin.tv_sec, x,
-                end.tv_sec, end.tv_usec,
-                diff.tv_sec, diff.tv_usec);
+                hwrecd.tv_sec, x,
+                udpend.tv_sec, w,
+                codecstart.tv_sec, y,
+                codecend.tv_sec, z,
+                codectime.tv_sec, codectime.tv_usec,
+                totaltime.tv_sec, totaltime.tv_usec,
+                s->hwrecd_current_avg,
+                encodertime.tv_sec, encodertime.tv_usec);
 #endif
 #ifdef __APPLE__
-            printf("%011d  %09lu.%06u  %09lu.%06u  %lu.%06u\n",
+            printf("%011d  %09lu.%06u  %09lu.%06u  %09lu.%06u  %09lu.%06u  %09lu.%06u  %09lu.%06u  %010.03f  %09lu.%06u\n",
                 frameNumber,
-                begin.tv_sec, x,
-                end.tv_sec, end.tv_usec,
-                diff.tv_sec, diff.tv_usec);
+                hwrecd.tv_sec, x,
+                udpend.tv_sec, w,
+                codecstart.tv_sec, y,
+                codecend.tv_sec, z,
+                codectime.tv_sec, codectime.tv_usec,
+                totaltime.tv_sec, totaltime.tv_usec,
+                s->hwrecd_current_avg,
+                encodertime.tv_sec, encodertime.tv_usec);
 #endif
 
             break;

4cf6fa8cc176aff1c3c1c173e4ace09147b8189d
Devin Heitmueller <dheitmueller@ltnglobal.com>
2019-08-15 16:53:22 -0400

    Move LTN feedback socket code to libavformat
    The original implementation for providing feedback to the LTN
    controller was baked into the decklink output module, but that
    prevents it from being used in other places.  Move the logic into
    libavformat where it can be shared with other callers.
    
    Worth noting that it's in libavformat instead of libavutil because
    libavutil doesn't have any of the platform independent abstractions
    for UDP sockets.

---
+++ libavformat/ltnlog.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019 LTN Global Communications Inc.
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file
+ * @ingroup lavu_ltn_log
+ * Public API for reporting events back to LTN's controller
+ */
+
+#ifndef AVUTIL_LTNLOG_H
+#define AVUTIL_LTNLOG_H
+
+int ltnlog_setup(const char *url);
+void ltnlog_stat(const char *str, uint64_t val);
+void ltnlog_msg(const char *msgtype, const char *fmt, ...);
+
+#endif /* AVUTIL_LTNLOG_H */

1cbf474e77a3bf3c370782464e20e5ee9150e15d
Devin Heitmueller <dheitmueller@ltnglobal.com>
2021-06-28 10:19:06 -0400

    Set lower bound for preroll to 3 frames
    Even if the user specifies an arbitrarily low preroll value, don't
    let the actual calculated preroll go below 3 frames.  This is
    because we with 1080p24/1080p25 we would end up with only two
    frames of preroll when the user set the preroll to 0.1 (since the
    calculation uses integer arithmetic).
    
    Having a preroll of two frames results in continuously reporting
    that the low watermark has been hit, but because the preroll is
    already two frames you end up "Advancing 0 frames" (since the
    insertion logic was intended to bring the buffer count back to the
    target preroll level).

--- libavdevice/decklink_enc.cpp
+++ libavdevice/decklink_enc.cpp
@@ -568,9 +568,15 @@ static int decklink_setup_video(AVFormatContext *avctx, AVStream *st)
     ctx->dlo->SetAudioCallback(ctx->output_callback);
     ctx->audio_samples_per_frame = bmdAudioSampleRate48kHz * st->time_base.num / st->time_base.den;
 
-    ctx->frames_preroll = st->time_base.den * ctx->preroll;
-    if (st->time_base.den > 1000)
-        ctx->frames_preroll /= 1000;
+    ctx->frames_preroll = ceil(st->time_base.den * ctx->preroll / st->time_base.num);
+    if (ctx->frames_preroll < 3) {
+        /* No matter what they specify as the preroll, we can't support lower than three
+           frames due to the way the hardware queueing works */
+        ctx->frames_preroll = 3;
+    }
+
+    /* Report the final preroll in milliseconds */
+    ltnlog_stat("PREROLL_TARGET", ctx->frames_preroll * st->time_base.num * 1000 / st->time_base.den);
 
     /* Buffer twice as many frames as the preroll. */
     ctx->frames_buffer = ctx->frames_preroll * 2;
